3b79e718e602a25c4a98a539e87d180104d55b56
Chris Gantz <chrisgantz836@gmail.com>
2018-07-30 12:41:03 -0700

    added get notes id item

--- server.js
+++ server.js
@@ -20,3 +20,9 @@ app.listen(8080, function () {
 app.get('/api/notes', (req, res) => {
   res.json(data);
 });
+
+app.get('/api/notes/:id', (req, res) => {
+  const id = req.params.id;
+  const item = data.find(item => item.id === Number(id));
+  res.json(item);
+});
