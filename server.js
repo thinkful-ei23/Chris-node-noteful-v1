e0d08fa9f0fa96b501937bf1583de1278f8d075f
Chris Gantz <chrisgantz836@gmail.com>
2018-07-31 16:34:58 -0700

    get notes by id finished

--- server.js
+++ server.js
@@ -30,10 +30,14 @@ app.get('/api/notes', (req, res, next) => {
   });
 });
 
-app.get('/api/notes/:id', (req, res) => {
-  const id = req.params.id;
-  const item = data.find(item => item.id === Number(id));
-  res.json(item);
+app.get('/api/notes/:id', (req, res, next) => {
+  const { id } = req.params;
+  notes.find(id, function(err, item) {
+    if (err) {
+      return next(err);
+    }
+    res.json(item);
+  });
 });
 // error handling
 app.get('/boom', (req, res, next) => {
