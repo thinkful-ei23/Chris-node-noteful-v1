eb16d9505f5c24e5a7274b6bda18ca96f6d11bc0
Chris Gantz <chrisgantz836@gmail.com>
2018-08-02 14:57:19 -0700

    everything works promise

--- public/scripts/noteful.js
+++ public/scripts/noteful.js
@@ -42,10 +42,16 @@ const noteful = (function () {
 
       const noteId = getNoteIdFromElement(event.currentTarget);
 
-      api.details(noteId, detailsResponse => {
-        store.currentNote = detailsResponse;
-        render();
-      });
+      api.details(noteId)
+        .this(details => {
+          store.currentNote = details;
+          render();
+        });
+
+      // api.details(noteId, detailsResponse => {
+      //   store.currentNote = detailsResponse;
+      //   render();
+      // });
 
     });
   }
@@ -57,10 +63,16 @@ const noteful = (function () {
       const searchTerm = $('.js-note-search-entry').val();
       store.currentSearchTerm = searchTerm ? { searchTerm } : {};
 
-      api.search(store.currentSearchTerm, searchResponse => {
-        store.notes = searchResponse;
-        render();
-      });
+      api.search(store.currentSearchTerm)
+        .then(response => {
+          store.notes = response;
+          render();
+        });
+
+      // api.search(store.currentSearchTerm, searchResponse => {
+      //   store.notes = searchResponse;
+      //   render();
+      // });
 
     });
   }
@@ -79,27 +91,40 @@ const noteful = (function () {
 
       if (noteObj.id) {
 
-        api.update(store.currentNote.id, noteObj, updateResponse => {
-          store.currentNote = updateResponse;
-
-          api.search(store.currentSearchTerm, searchResponse => {
-            store.notes = searchResponse;
+        api.update(store.currentNote.id, noteObj)
+          .then(updateRes => {
+            store.currentNote = updateRes;
+            return api.search(store.currentSearchTerm);
+          })
+          .then(searchRes => {
+            store.notes = searchRes;
             render();
           });
+          
 
-        });
+        // api.update(store.currentNote.id, noteObj, updateResponse => {
+        //   store.currentNote = updateResponse;
 
+        //    api.search(store.currentSearchTerm, searchResponse => {
+        //     store.notes = searchResponse;
+        //     render();
+        //   });
       } else {
 
-        api.create(noteObj, createResponse => {
-          store.currentNote = createResponse;
-
-          api.search(store.currentSearchTerm, searchResponse => {
-            store.notes = searchResponse;
-            render();
+        api.create(noteObj)
+          .then(createRes => {
+            store.currentNote = createRes;
           });
 
-        });
+        // api.create(noteObj, createResponse => {
+        //   store.currentNote = createResponse;
+
+        //   api.search(store.currentSearchTerm, searchResponse => {
+        //     store.notes = searchResponse;
+        //     render();
+        //   });
+
+        // });
       }
     });
   }
@@ -121,15 +146,25 @@ const noteful = (function () {
 
       const noteId = getNoteIdFromElement(event.currentTarget);
 
-      api.remove(noteId, () => {
-        api.search(store.currentSearchTerm, searchResponse => {
+
+      api.remove(noteId)
+        .then(() => api.search(store.currentSearchTerm))
+        .then(searchResponse => {
           store.notes = searchResponse;
           if (noteId === store.currentNote.id) {
             store.currentNote = {};
           }
           render();
         });
-      });
+      // api.remove(noteId, () => {
+      //   api.search(store.currentSearchTerm, searchResponse => {
+      //     store.notes = searchResponse;
+      //     if (noteId === store.currentNote.id) {
+      //       store.currentNote = {};
+      //     }
+      //     render();
+      //   });
+      // });
     });
   }
 
@@ -145,7 +180,7 @@ const noteful = (function () {
   // This object contains the only exposed methods from this module:
   return {
     render: render,
-    bindEventListeners: bindEventListeners,
+    bindEventListeners: bindEventListeners
   };
 
 }());
